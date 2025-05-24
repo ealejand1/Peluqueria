ddd5af5a7567a03ea56fc1d1e96d7390334c699f
Erik Alejandro Quezada <erikalejjandro1112@gmail.com>
2025-05-24 02:48:04 +0200

    Creacion de Componentes
    Componentes cabecera,cuerpo y reserva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { CuerpoComponent } from './componentes/cuerpo/cuerpo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CabeceraComponent,CuerpoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
