7fdcdcad48e37b756143869d1277bda0a3eb77d9
Erik Alejandro Quezada <erikalejjandro1112@gmail.com>
2025-06-17 11:50:56 +0200

    Creacion del Componente ADMIN

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { RegistroPagComponent } from './paginas/registro-pag/registro-pag.component';
 import { HomeComponent } from './paginas/home/home.component';
 import { IniciarsesionComponent } from './paginas/iniciarsesion/iniciarsesion.component';
+import { AdminComponent } from './paginas/admin/admin.component';
 
 
 export const routes: Routes = [
     {path:'',component: HomeComponent},
     {path:'registro',component:RegistroPagComponent},
-    {path:'login',component:IniciarsesionComponent}
+    {path:'login',component:IniciarsesionComponent},
+    {path:'admin',component:AdminComponent}
 ];
